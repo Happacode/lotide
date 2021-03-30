0883bcde4ac11c3c3134c64b29b7f4f781147426
Harp Sandhu <happacode@protonmail.com>
2021-03-29 20:11:50 -0700

    Implement AsserEqual

---
+++ assertEqual.js
@@ -0,0 +1,12 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ ✅ ✅  Assertion passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑 🛑 🛑  Assertion failed: ${actual} !== ${expected}`);
+  }
+};
+
+// TEST CODE
+console.log(assertEqual("Lighthouse Labs", "Bootcamp"));
+console.log(assertEqual(1, 1));
