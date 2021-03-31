1a5e5e101bc8c8fae40ddcb380be4335093130a5
Harp Sandhu <happacode@protonmail.com>
2021-03-31 13:54:36 -0700

    Implementing a function that can compare two arrays for a perfect match.

--- eqArrays.js
+++ eqArrays.js
@@ -6,20 +6,16 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-const eqArrays = function(actual, expected) {
-  if (actual.length === expected.length) {
-    let perMatch = true;
-    for (let i = 0; i < actual.length; i++) {
-      if (actual[i] !== expected[i]) {
-        perMatch = false;
-      }
-    }
-    if (perMatch) {
-      return true;
-    } else {
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  for (let i = 0; i < array1.length; i++) {
+    if (array1 [i] !== array2[i]) {
       return false;
     }
   }
+  return true;
 };
 
 console.log(assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true)); // => should PASS
