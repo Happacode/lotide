d57577ef7f7be7f0d8161a247a1258c95f9195c3
Harp Sandhu <happacode@protonmail.com>
2021-04-05 12:56:31 -0700

    Refactor Lotide with module.export and require, plus test files.

---
+++ test/headTest.js
@@ -0,0 +1,7 @@
+// test/headTest.js
+const assertEqual = require('../assertEqual');
+const head = require('../head');
+
+// Test code
+console.log(assertEqual(head([5,6,7]), 5));
+console.log(assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello"));
