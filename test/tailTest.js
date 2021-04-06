f5acefb2c8187c419a42978ee4a58b3397f9d048
Harp Sandhu <happacode@protonmail.com>
2021-04-05 23:03:36 -0700

    Refactored module.export and require

---
+++ test/tailTest.js
@@ -0,0 +1,12 @@
+const assertEqual = require('../assertEqual');
+const tail = require('../tail');
+
+// Test Case 1: Check the returned array elements
+const result = tail(["Hello", "Lighthouse", "Labs"]);
+console.log(assertEqual(result.length, 2)); // ensure we get back two elements
+console.log(assertEqual(result[0], "Lighthouse")); // ensure first element is "Lighthouse"
+console.log(assertEqual(result[1], "Labs")); // ensure second element is "Labs"
+
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words); // no need to capture the return value since we are not checking it
+console.log(assertEqual(words.length, 3)); // original array should still have 3 elements!
